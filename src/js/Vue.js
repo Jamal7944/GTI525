623a8f63f731f56b8156b834f8b361bae179876f
BigManAwesome <steven_l_l1@hotmail.com>
2024-05-30 15:35:56 -0400

    vue de données est terminé

--- src/js/Vue.js
+++ src/js/Vue.js
@@ -1,5 +1,6 @@
 import Papa from 'papaparse'; // Importez Papaparse
-import { StationData } from './stationData';
+//import { StationData } from './stationData';
+import { vueDonnees } from './vueDonnees';
 
 export default {
   name: "App",
@@ -11,7 +12,10 @@ export default {
       years: [],
       monthName: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre",
         "Octobre", "Novembre", "Decembre"], 
+      vueDonnees_TableHeader: ["Années", "Mois", "Temps Moyen Max (C)", "Temps Moyen Min (C)", "Temps Moyen (C)", "Temps Max Enregistré (C)",
+    "Temps Min Enregistré (C)", "Pluie(mm)", "Neige(cm)", "Vitesse du vent(km/h)"],   
     };
+    
   },
 
   mounted() {
@@ -78,17 +82,32 @@ export default {
       this.csvData.forEach((i) => {
         let fieldArray = [];
         for(let k in i) {
-          fieldArray.push(i[k]);
+            fieldArray.push(i[k]);
         }
+        let year = fieldArray[5];
+        let month = fieldArray[6];
+        let temp_Mean_Max =fieldArray[7];
+        let temp_Mean_Min= fieldArray[9];
+        let temp_Mean= fieldArray[11];
+        let temp_Max = fieldArray[13];
+        let temp_Min = fieldArray[15];
+        let rain= fieldArray[17];
+        let snow= fieldArray[19];
+        let wind_Speed_Max = fieldArray[27];
 
-        console.log("Iteration: "+fieldArray[5]);
 
-        console.log(fieldArray);
+       /* console.log("Year: "+year+", Month: "+month+", Mean Max: "+temp_Mean_Max+", Mean Min: "+temp_Mean_Min +", Temp Mean: "+temp_Mean+
+        ", Temp Max: "+ temp_Max+", Temp Min: "+temp_Min+", Rain: "+rain+", Snow: "+snow+", Wind: "+wind_Speed_Max);*/
+        let arrayData = [year, month, temp_Mean_Max, temp_Mean_Min, temp_Mean, temp_Max, temp_Min, rain, snow, wind_Speed_Max];
+        
+        console.log(arrayData);
+        //console.log(fieldArray);
         if(fieldArray.length != 29) {
           console.log(fieldArray);
         }
         else {
-          let data = new StationData(fieldArray);
+          //let data = new StationData(fieldArray);
+          let data = new vueDonnees(arrayData);
           this.stnData.push(data);
           //console.log("data: "+data);
         }
