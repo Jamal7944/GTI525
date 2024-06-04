19d00ba41411f85c5d974f6ff9abad1ede71f6b5
Jamal@2 <86193096+Jamal7944@users.noreply.github.com>
2024-06-03 21:28:14 -0400

    Correction affichage province

--- src/js/NavigationMenu.js
+++ src/js/NavigationMenu.js
@@ -18,6 +18,10 @@ export default {
         const response = await fetch("/Laboratoire_1_-_Enonces-20240516/Lab1_CSV/Station Inventory EN.csv");
         const csvText = await response.text();
         this.ListeStations = Papa.parse(this.removeFirstLines(csvText,3), { header: true }).data;
+        this.ListeStations = this.ListeStations.filter((item, i, ar) => {
+                    return ar.findIndex(obj => obj.Province === item.Province) === i;
+      });
+      
         console.log(this.ListeStations)
       } catch (error) {
         console.error("Erreur lors du chargement du fichier CSV :", error);
