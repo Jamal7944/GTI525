699483b9d185c3489b0b8c7c2ca645876ffafe46
Jamal@2 <86193096+Jamal7944@users.noreply.github.com>
2024-06-06 13:57:35 -0400

    Save commit

--- src/js/NavigationMenu.js
+++ src/js/NavigationMenu.js
@@ -5,7 +5,8 @@ export default {
   name: "NavigationMenu",
   data() {
     return {
-      ListeStations:[]
+      ListeStations:[],
+      ProvinceStations:[],
     };
   },
   mounted() {
@@ -18,12 +19,18 @@ export default {
         const response = await fetch("/Laboratoire_1_-_Enonces-20240516/Lab1_CSV/Station Inventory EN.csv");
         const csvText = await response.text();
         this.ListeStations = Papa.parse(this.removeFirstLines(csvText,3), { header: true }).data;
-        this.ListeStations = this.ListeStations.filter((item, i, ar) => {
-                    return ar.findIndex(obj => obj.Province === item.Province) === i;
-      });
+
       
-        console.log(this.ListeStations)
-      } catch (error) {
+        this.ListeStations.forEach(station => {
+          const province = station.Province;
+          if (!this.ProvinceStations[province]) {
+            this.ProvinceStations[province] = [];
+          }
+          this.ProvinceStations[province].push(station);
+      });
+
+
+    } catch (error) {
         console.error("Erreur lors du chargement du fichier CSV :", error);
       }
     },
