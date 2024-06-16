b63811260d0c1fc59b526172fe681b1e89e743b7
Jamal@2 <86193096+Jamal7944@users.noreply.github.com>
2024-06-16 18:10:18 -0400

    Correction

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-// Exemple de route GET pour '/api/data'
 router.get('/', (req, res) => {
   // Renvoyer des données en tant que réponse JSON
   res.json({ message: 'Exemple de données depuis une route API avec GET' });
 });
 
-// Exemple de route POST pour '/api/postData'
 router.post('/', (req, res) => {
   // Récupérer les données envoyées via la requête POST
   // Effectuer des opérations avec les données reçues
