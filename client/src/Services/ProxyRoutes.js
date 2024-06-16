6b6f4a908089cf726f0fb19c83f55e7b26de7586
Jamal@2 <86193096+Jamal7944@users.noreply.github.com>
2024-06-16 17:42:05 -0400

    Back End Server Integration

---
+++ client/src/Services/ProxyRoutes.js
@@ -0,0 +1,22 @@
+// proxyRoutes.js
+
+const proxyRoutes = {
+    '/api': {
+      target: 'http://localhost:8081/api',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    },
+    '/other': {
+      target: 'http://localhost:8081/other',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/other': ''
+      }
+    },
+    // Ajoutez d'autres routes selon vos besoins
+  };
+  
+export default proxyRoutes;
+  
--- src/components/App.vue
+++
@@ -1,7 +0,0 @@
-
-<template src="../html/Vue.html"></template>
-
-<script src="../js/Vue.js"></script>
-
-<style src="../css/Vue.css"></style>
-<style src="../css/main.css"></style>
